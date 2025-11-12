996b2dbc680eebf130e83d691ae90a192b2dea0e
OstinT <145394220+OstinT66@users.noreply.github.com>
2025-11-12 16:34:23 -0500

    Add files via upload

---
+++ 03_BASE_DATOS/bd.js
@@ -0,0 +1,33 @@
+let comic = {
+  "nombreComic": "Eco olvidado",
+  "numeroCapitulos": "3",
+  "genero": "....",
+  "sipnosis": "...",
+  "autores": ["Santiago", "Luis", "Salome"],
+  "portadaComic": "imagen.jpg",
+  "year": 2025,
+  "capitulos": [
+    {
+        "id": 1,
+      "nombre": "Capítulo 1",
+     "personajes": ["..", "...", "...."],
+     "portada": "imagen1.jpg",
+     "descripcion": "..."
+    },
+    {
+   "id": 2,
+      "nombre": "Capítulo 2",
+     "personajes": ["..", "...", "...."],
+     "portada": "imagen1.jpg",
+     "descripcion": "..."
+      
+    },
+    {
+      "id": 3,
+      "nombre": "Capítulo 3",
+     "personajes": ["..", "...", "...."],
+     "portada": "imagen1.jpg",
+     "descripcion": "..."
+    }
+  ]
+}
