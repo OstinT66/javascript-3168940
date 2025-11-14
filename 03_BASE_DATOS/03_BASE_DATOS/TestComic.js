62bcc5a5847a8bf06f16fc52179dc982ed9aaf62
OstinT <145394220+OstinT66@users.noreply.github.com>
2025-11-14 17:01:38 -0500

    Add files via upload

---
+++ 03_BASE_DATOS/03_BASE_DATOS/TestComic.js
@@ -0,0 +1,24 @@
+/* const infocomic = document.querySelector(".info-comic");
+
+console.log(infocomic);
+
+infocomic.innerHTML = `
+    <small>${comic.year}</small>
+    <h1>${comic.nombreComic}</h1>
+    <p>${comic.sinopsis}</p>
+    <p>${comic.genero}</p>
+`
+ */
+const hero = document.querySelector(".hero");
+
+console.log(hero);
+
+hero.innerHTML = `
+          <h1>${comic.nombreComic}</h1>
+          <p>${comic.sinopsis}</p>
+          <div class="actions">
+            <button class="btn btn-primary js-start">Comenzar</button>
+            <a class="btn btn-ghost" href="#episodes">Watch</a>
+          </div>
+          <p>${comic.year}</strong></p>
+          `
